75fe5abf4a9eb287bb13a06c3f5ec5f6ef864f6f
Cheffjoker <daniel.arroyave.22@gmail.com>
2021-10-21 00:46:51 +0200

    Lematizador pre curl

--- prelematizador.py
+++ prelematizador.py
@@ -1,36 +1,37 @@
 import re
 import nltk
-import multiprocessing
-from gensim.models import Word2Vec
-from unicodedata import normalize
 from nltk.corpus import stopwords
 from time import time
 from sys import getsizeof
 import os
 import logging
-import sys
-import codecs
+import subprocess
+
+nada = input("Y tu que miras payaso")
+nada = input("que coño quieres ")
+nada = input("te lematizo o que ")
+nada = input("que me ANSIo  ")
+telematizo = input("que quieres que te lematize primero? ")
 
 logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s", datefmt= '%H:%M:%S',filename='.\log\log.log', level=logging.DEBUG)
 corpus = [archivo for archivo in  os.listdir(os.getcwd()) if archivo[-3:] == 'txt']
 corpusTXT = open('.\corpus\corpusTXT.txt', 'w')
-BibliaCoranKitabi = open('.\BibliaCoranKitabi_SW.txt', 'w')
+Prelematizado = open(telematizo+'_SW.txt', 'w')
 texto = ''
 cont = 1
 t_general = time()
 t = time()
 for archivo in corpus:
 
-    txt = open(archivo, 'r').read()
-#    txt = txt.lower()
-#    txt = re.sub('[0-9]|[\(]|[\)]|[#]|[-]|[\[]|[\]]|[\']|[¿]|[?]|[¡]|[!]|[,]|[.]|[:]|[;]', ' ', txt)
+    txt = open(telematizo+'.txt', 'r').read()
+    txt = txt.lower()
+    txt = re.sub('[0-9]|[\(]|[\)]|[#]|[-]|[\[]|[\]]|[\']|[¿]|[?]|[¡]|[!]|[,]|[.]|[:]|[;]', ' ', txt)
     corpusTXT.write(txt)
-
+    cont=+1
     logging.debug('[LEM]-->van '+ str(cont) +' libros normalizados en memoria')
+
 corpusTXT.close()
-
 corpusTXT = open('.\corpus\corpusTXT.txt', 'r')
-
 texto = corpusTXT.read()
 
 SizeDataMb = getsizeof(texto)/1000000
@@ -52,16 +53,17 @@ logging.debug('[LEM]-->Tiempo de Tokenizacion de palabras: {} mins'.format(round
 t = time()
 for i in range(len(palabras)):
     palabras[i] = [w for w in palabras[i] if w not in stopwords.words('spanish')]
-    BibliaCoranKitabi.write(' '.join([str(item) for item in palabras[i]]))
-    BibliaCoranKitabi.write('\n')
+    Prelematizado.write(' '.join([str(item) for item in palabras[i]]))
+    Prelematizado.write('\n')
     print(palabras[i])
 
 logging.debug('[LEM]-->Retiradas palabras de parada')
 logging.debug('[LEM]-->Tiempo de Retiradas palabras de parada: {} mins \n'.format(round((time() - t) / 60, 2)))
 
-BibliaCoranKitabi.close()
+Prelematizado.close()
 logging.debug('[LEM]-->Tiempo de ejecución completa del lematizador: {} mins'.format(round((time() - t_general) / 60, 2)))
 
+#subprocess.call('curl -F file=@BibliaCoranKitabi_SW.txt "http://www.corpus.unam.mx/servicio-freeling/analyze.php?outf=tagged&format=plain" -o BCK_Lematizado.txt', shell = True)
 
 
 
@@ -70,4 +72,7 @@ logging.debug('[LEM]-->Tiempo de ejecución completa del lematizador: {} mins'.f
 
 
 #LEMATIZADOR :   http://www.corpus.unam.mx/servicio-freeling
-#                curl -F file=@BibliaCoranKitabi_SW.txt "http://www.corpus.unam.mx/servicio-freeling/analyze.php?outf=tagged&format=plain" -o BCK_Lematizado.txt
+
+#                iconv -f ANSI_X3.110 -t UTF-8 -o BibliaCoranKitabi_SW.TXT
+#                curl -F 'file=@BibliaCoranKitabi_SW.txt' "http://www.corpus.unam.mx/servicio-freeling/analyze.php?outf=tagged&format=plain" -o BCK_Lematizado.txtgit.txt
+
